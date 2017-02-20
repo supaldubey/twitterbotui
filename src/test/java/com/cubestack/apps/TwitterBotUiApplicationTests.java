3f3cb082dded981c7156f08e77e892cb3b384d2f
supal_dubey <supal.dubey@gmail.com>
2017-02-20 16:34:55 +0530

    Initial setup

---
+++ src/test/java/com/cubestack/apps/TwitterBotUiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cubestack.apps;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TwitterBotUiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
