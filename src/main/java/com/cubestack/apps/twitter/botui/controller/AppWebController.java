1d42a7bc42f6a75c2d6fddb025976ba931bccef4
Supal Dubey <supal.dubey@gmail.com>
2017-02-21 21:47:20 +0530

    Pagination fixes

--- src/main/java/com/cubestack/apps/twitter/botui/controller/AppWebController.java
+++ src/main/java/com/cubestack/apps/twitter/botui/controller/AppWebController.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
